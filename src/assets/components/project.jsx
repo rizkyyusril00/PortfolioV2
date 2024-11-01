35ca4b357e0594131e5c3a2d1848e0ab7464f1b7
rizkyyusril <rizkyyusril74@gmail.com>
2024-11-01 16:48:54 +0700

    add new project card

--- src/assets/components/project.jsx
+++ src/assets/components/project.jsx
@@ -1,9 +1,11 @@
 import LogoTailwind from "../img/tailwind-logo.svg"
+import LogoNextJs from "../img/nextJs.png"
 import Project1 from '../img/project_1.png'
 import Project2 from '../img/project_2.png'
 import Project3 from '../img/project_3.png'
 import Project4 from '../img/project_4.png'
 import Project5 from '../img/project_5.png'
+import Project6 from '../img/project_6.png'
 
 
 export default function Project(){
@@ -89,7 +91,7 @@ export default function Project(){
                 </div>
 
                 {/* 3 */}
-                <div className="flex justify-center gap-2">
+                <div className="flex gap-2">
                     {/* 1 */}
                     <a href="https://mangstore-react.vercel.app/" target="_blank" className="flex flex-col gap-4 border border-primary rounded-md p-4 w-1/2 group hover:bg-primary hover:border-bg transition-all duration-500 ease-in-out overflow-hidden">
                         <figure className="bg-white w-full">
@@ -102,6 +104,24 @@ export default function Project(){
                         <div className='flex gap-2'>
                             <figure className="w-[20px] h-[20px] lg:w-6 lg:h-6 xl:w-7 xl:h-7">
                                 <img src={LogoTailwind } alt="" className="w-full h-full object-fit"/>
+                            </figure>
+                            <i className="fa-brands fa-react text-[20px] lg:text-[24px] xl:text-[28px] text-[#00d8ff]"></i>
+                        </div>
+                    </a>
+                    <a href="https://mentalhealing.id/home" target="_blank" className="flex flex-col gap-4 border border-primary rounded-md p-4 w-1/2 group hover:bg-primary hover:border-bg transition-all duration-500 ease-in-out overflow-hidden">
+                        <figure className="bg-white w-full md:h-auto aspect-video">
+                            <img src={Project6} className='w-full h-full transition-all duration-500 ease-in-out hover:scale-110' alt="certificate1" />
+                        </figure>
+                        <div className="flex gap-2">
+                            <i className="bi bi-6-circle text-[16px] lg:text-[20px] xl:text-[24px] text-primary group-hover:text-bg"></i>
+                            <h2 className="text-[16px] lg:text-[20px] xl:text-[24px] text-primary group-hover:text-bg">Web App For Mental Health</h2>
+                        </div>
+                        <div className='flex gap-2'>
+                            <figure className="w-[20px] h-[20px] lg:w-6 lg:h-6 xl:w-7 xl:h-7">
+                                <img src={LogoTailwind } alt="" className="w-full h-full object-fit"/>
+                            </figure>
+                            <figure className="w-[20px] h-[20px] lg:w-6 lg:h-6 xl:w-7 xl:h-7">
+                                <img src={LogoNextJs } alt="" className="w-full h-full object-fit"/>
                             </figure>
                             <i className="fa-brands fa-react text-[20px] lg:text-[24px] xl:text-[28px] text-[#00d8ff]"></i>
                         </div>
