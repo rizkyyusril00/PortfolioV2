7f0a5768eb63eef377c4d64c8ffced8bb7a17e2e
rizkyyusril <rizkyyusril74@gmail.com>
2024-11-05 13:56:30 +0700

    audit

--- src/assets/components/home-components/homeBox1.jsx
+++ src/assets/components/home-components/homeBox1.jsx
@@ -5,20 +5,20 @@ export default function HomeBox1() {
                 <h2 className="text-[28px] slg:text-[36px] lg:text-[40px] xl:text-[44px] text-primary">
                 Supp, Im <span>FrontEnd Developer</span>
                 </h2>
-                <ul className="flex items-center text-[16px] text-primary gap-10">
+                <div className="flex items-center text-[16px] text-primary gap-10">
                     <div className="flex items-center">
                         <i className="bi bi-dot text-[28px] slg:text-[36px] lg:text-[40px] xl:text-[44px] text-primary"></i>
-                        <li className="decoration-dotted text-[12px] slg:text-[16px] lg:text-[20px] xl:text-[24px] text-primary">
+                        <span className="decoration-dotted text-[12px] slg:text-[16px] lg:text-[20px] xl:text-[24px] text-primary">
                         Lived In Sukabumi
-                        </li>
+                        </span>
                     </div>
                     <div className="flex items-center">
                         <i className="bi bi-dot text-[28px] slg:text-[36px] lg:text-[40px] xl:text-[44px] text-primary"></i>
-                        <li className="decoration-dotted text-[12px] slg:text-[16px] lg:text-[20px] xl:text-[24px] text-primary">
+                        <span className="decoration-dotted text-[12px] slg:text-[16px] lg:text-[20px] xl:text-[24px] text-primary">
                         Currently Studying at University
-                        </li>
+                        </span>
                     </div>
-                </ul>
+                </div>
                 <div className="bg-transparent w-full flex flex-col gap-1">
                     <p className="text-primary text-[12px] slg:text-[16px] lg:text-[20px] xl:text-[24px] leading-tight">
                         Very interested to working in the field of technology, especially
--- src/assets/components/home-components/homeBox2.jsx
+++ src/assets/components/home-components/homeBox2.jsx
@@ -54,7 +54,7 @@ export default function HomeBox2() {
                     {/* box kanan */}
                     <div className="flex items-center justify-center">
                         <button className="cvBtn px-1 py-1 lg:py-2">
-                            <a  href={CV} download={CV} className="IconContainer">
+                            <a href={CV} aria-label="my-cv" download={CV} className="IconContainer">
                                 <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className="bi bi-download icon w-[16px] h-[16px] xl:w-[20px] xl:h-[20px]" viewBox="0 0 16 16">
                                     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
                                     <path className="animate-bounce " d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
