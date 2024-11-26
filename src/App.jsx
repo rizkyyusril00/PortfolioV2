7f0946dcf5f67e75571cc2bce4bd339c84671b68
rizkyyusril <rizkyyusril74@gmail.com>
2024-11-26 11:08:15 +0700

    add new package bs icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import Main from "./assets/components/main"
-import ParticlesComponent from "./assets/components/particles"
-import { useState } from "react"
-
+import Main from "./assets/components/main";
+import ParticlesComponent from "./assets/components/particles";
+import { useState } from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
-  const [value, setValue] = useState(1)
+  const [value, setValue] = useState(1);
   const togleClick = () => {
-  
-    if (value % 2 === 1) { // Check if odd click (first click or every other odd click)
+    if (value % 2 === 1) {
+      // Check if odd click (first click or every other odd click)
       setValue(value + 1); // Increment value on odd clicks
     } else {
       setValue(value - 1); // Decrement value on even clicks
     }
   };
-  
+
   // blob show
-  const [blobShow, setBlobShow] = useState("Home")
+  const [blobShow, setBlobShow] = useState("Home");
 
   // navigation sidebar menus
-  const [navMenu, setNavMenu] = useState("Home")
+  const [navMenu, setNavMenu] = useState("Home");
   const handleMenuHome = () => {
     setNavMenu("Home");
     setBlobShow("Home");
@@ -36,15 +36,23 @@ function App() {
     setBlobShow("Certificate");
   };
 
-
   return (
     <>
       <div className={value === 1 ? "bg-transparent" : "bg-bg"}>
-        <ParticlesComponent  id={value === 1 ? "particles" : "particles2"}/>
-        <Main value={value} togleClick={togleClick} blobShow={blobShow} navMenu={navMenu} handleMenuHome={handleMenuHome} handleMenuAbout={handleMenuAbout} handleMenuProject={handleMenuProject} handleMenuCertificate={handleMenuCertificate}/>
+        <ParticlesComponent id={value === 1 ? "particles" : "particles2"} />
+        <Main
+          value={value}
+          togleClick={togleClick}
+          blobShow={blobShow}
+          navMenu={navMenu}
+          handleMenuHome={handleMenuHome}
+          handleMenuAbout={handleMenuAbout}
+          handleMenuProject={handleMenuProject}
+          handleMenuCertificate={handleMenuCertificate}
+        />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
