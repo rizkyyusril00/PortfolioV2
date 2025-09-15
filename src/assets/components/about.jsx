565c608ed67ef2628b7366c98519cf5ffc8c44eb
Rizky Yusril <rizkyyusril74@gmail.com>
2025-09-15 15:37:34 +0700

    update about

--- src/assets/components/about.jsx
+++ src/assets/components/about.jsx
@@ -9,7 +9,7 @@ export default function About(){
             <AboutBox1 />
             <div className="flex flex-col items-center gap-10 py-8 border-t border-dashed border-primary">
                 {/* atas */}
-                <AboutContent content="A student of Nusa Putra University, S1 Information Systems Study Program, I have found joy in my challenging academic journey. As an individual who enjoys learning new things, my confidence is tested through leadership roles in leading various projects and managing events."/>
+                <AboutContent content="Bachelor of Computer Science, majoring in Information Systems, Nusa Putra University. I have found joy in my challenging academic journey. As an individual who enjoys learning new things, my confidence is tested through leadership roles in leading various projects and managing events."/>
                 {/* bawah */}
                 <AboutContent content="Over the course of my studies, my interest has grown in the areas of project management, UI &UX, and website development more specifically in frontend developers. I realize the importance of detail in achieving success, and thats what I focus on every step I take. I am committed to continuing to hone my skills and knowledge!"/>
             </div>
